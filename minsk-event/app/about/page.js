8579e3001e337d1bd637757cf18e8f8acb8e48f0
Егор <matylitski.egor.99@gmail.com>
2025-09-08 22:41:31 +0300

    first commit

---
+++ minsk-event/app/about/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function about() {
+  return (
+    <div>
+      <p className="text-blue-600 text-9xl">
+        Этот сайт создан двумя энтузиастами чтобы вы всегда знали куда сходить
+      </p>
+    </div>
+  );
+}
+
+export default about;
---
+++ minsk-event/app/page.js
@@ -0,0 +1,9 @@
+import './globals.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-8xl">Minsk Event</h1>
+    </div>
+  );
+}
